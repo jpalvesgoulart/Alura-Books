10ab2fbf26af8aa67b78a3cfea179da17ae81676
jpalvesgoulart <jpalvesgoulart@outlook.com>
2023-04-11 17:41:18 -0300

    Adicionando formulário e consumindo API de CEP

---
+++ js/script.js
@@ -0,0 +1,35 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = ``;
+    try{
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro){
+        throw Error('CEP não existente!')
+    }
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+    var bairro = document.getElementById('bairro');
+    var contato = document.getElementById('telefone');
+    var complemento = document.getElementById('complemento');
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+    bairro.value = consultaCEPConvertida.bairro;
+    contato.value = consultaCEPConvertida.ddd;
+    complemento.value = consultaCEPConvertida.complemento;
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida
+    } catch(erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+        console.log(erro)
+    }
+} 
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
+
+
